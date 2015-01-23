3555d9c0e9d7ece48f061403adffad4e7b463b60
nrh217 <nrh217@lehigh.edu>
2015-01-23 16:37:13 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+//Nick Hochrein
+//Lab 01
+//Hello World Java Program
+//
+//defina a class
+public class HelloWorld{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//print the statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
