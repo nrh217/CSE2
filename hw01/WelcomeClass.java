d5889ad73230a505b54289d406b939cf511c7824
nrh217 <nrh217@lehigh.edu>
2015-01-27 21:44:46 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Nick Hochrein
+//HW01
+//Welcome Class Java Program
+//
+//define a class
+public class WelcomeClass{
+    
+//add main method
+    public static void main(String[] args) {
+        
+//print the statement
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");  
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\  ");
+        System.out.println("<-N--R--H--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");  
+        System.out.println("I missed last semester because I was in a car accident");
+        System.out.println("that left me in the hospital for 4 months");
+    }
+}
