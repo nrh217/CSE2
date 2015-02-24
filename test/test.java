9331421dbbf0bb20de356a48bd51f3a432c74a36
nrh217 <nrh217@lehigh.edu>
2015-02-24 17:02:41 +0000

    updated SpaceExploration

---
+++ test/test.java
@@ -0,0 +1,25 @@
+//define a class
+public class test{
+        
+//add main method
+    public static void main(String[] args) {
+  int i;
+i = 3;
+switch(i){
+	case 1:
+	case 2:
+	case 3:
+		System.out.println("hello!");
+	case 4:
+		System.out.println("goodbye!");
+		break;
+	case 5:
+	case 3:
+		System.out.println("testing!");
+		break;
+	default: 
+		System.out.println("default!");
+		break;
+}
+}
+}
