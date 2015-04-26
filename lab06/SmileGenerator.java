dd1ebb02e59a7e04688fa557cfe9e8f1dbe6a569
nrh217 <nrh217@lehigh.edu>
2015-04-26 17:36:08 +0000

    updated SmileGenerator

--- lab06/SmileGenerator.java
+++ lab06/SmileGenerator.java
@@ -9,34 +9,60 @@ public class SmileGenerator{
 //add main method
     public static void main(String[] args){
         
-//create loop to print out smileys
-       int counter=0;
-       int nSmileys=(int)(Math.random()*100)+1;
-       int counter2=1;
-       int counter3=0;
-       System.out.println(":):):):):)");
-       while(counter<5){
-            System.out.print(":)");
-            counter+=1;
+//create for loop to generate 5 smileys
+        int i;
+        for(i=0; i<5;i++){
+            System.out.print(":) ");
         }
-        while(counter<nSmileys){
-            System.out.print(":)");
-            counter+=1;
-            if(counter%30 == 0){
-                System.out.println("");
-                counter+=1;
-            }
-            while(counter==counter2){
-                System.out.println("");
-                counter2=counter2+2+counter3;
-                counter3+=1;
-            }
+        System.out.println("");
+        System.out.println("");
+//create 5 in while loop
+        i=0;
+        while(i<5){
+         System.out.print(":) ");
+         i++;
         }
-        
-        
-        
-        
-        
+        System.out.println("");
+        System.out.println("");
+//create 5 in do-while loop
+        i=0;
+        do{
+            System.out.print(":) ");
+            i++;
+        }while(i<5);
+        System.out.println("");
+         System.out.println("");
+//create between 1 and 100 smiley faces
+        int val=((int)(Math.random()*100))+1;
+        for(i=0;i<val;i++){
+            System.out.print(":) ");
+        }
+        System.out.println("");
+        System.out.println("");
+//limit smileys for random to 30 per line
+        int counter;
+        for(i=0;i<val;i=i){
+            for(counter=0;counter<30;counter++){
+                System.out.print(":) ");
+                i++;
+                if(i>=val){
+                    break;
+                }
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+//create loop with each line containing one more smiley
+        counter=1;
+        for(i=0;i<val;i=i){
+            for(int j=0;j<counter;j++){
+                System.out.print(":) ");
+                i++;
+            }
+            counter++;
+            System.out.println("");
+        }
+        System.out.println("");
         
         
     }
