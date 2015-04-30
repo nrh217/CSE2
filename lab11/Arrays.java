1d716dc91fa824b00353a8ca7bc157f160ed397e
nrh217 <nrh217@lehigh.edu>
2015-04-30 15:42:32 +0000

    updated lab 11

--- lab11/Arrays.java
+++ lab11/Arrays.java
@@ -72,35 +72,30 @@ public class Arrays{
             }
         }
         System.out.println("The maximum value of array1 is "+max1+" and the minimum value is "+min1);
-        for(int k=0; k<50; k++){
-            array2[k]=(int)(Math.random()*101);
-        }
-        int min2=array2[0];
-        int index=0;
-        for(int l=0; l<50; l++){
-            for(int m=1; m<50; m++){
-                if(array2[m]<min2){
-                    min2=array2[m];
-                    index=m;
-                }
-            }
-            int temp=array2[l];
-            array2[l]=array2[index];
-            array2[index]=temp;
+        array2[0]=(int)(Math.random()*101);
+        for(int k=1; k<50; k++){
+            array2[k]=((int)(Math.random()*101))+array2[k-1];
         }
         System.out.println("The maximum value of array2 is "+array2[49]+" and the minimum value is "+array2[0]);
+        for(int f=0;f<array2.length;f++){
+            System.out.print(" "+array2[f]);
+        }
+        System.out.println("");
         int target=0;
         System.out.println("Enter an int>=0");
         double num=myScanner.nextDouble();
+        
         if((num%1)!=0||num<0){
             System.exit(0);
         }
+        
         else{
             target=(int)num;
         }
         int z=0;
+        int[] y=search(array2,target);
         while(z==0){
-            int[]y=search(array2,target);
+            y=search(y,target);
             if(y[0]==-1){
                 z++;
             }
--- lab12/Matrices.java
+++ lab12/Matrices.java
@@ -10,7 +10,29 @@ import java.util.scanner;
 public class Matrices{
     
 //Define increasing Matrix method
-    public static int[]
+    public static int[][] increasingMatrix(int width, int height, boolean format){
+        int[][]array=new int[3][3];
+        int input=1;
+        if(format=true){
+            for(int i=0;i<array.length;i++){
+                for(int j=0;j<array[i].length;j++){
+                    array[i][j]=input;
+                    input++;
+                }
+            }
+        }
+        else{
+            int input2=1;
+            for(int a=0;a<array.length;a++){
+                for(int b=0; b<array[i].length;b++){
+                    array[i][j]=input2;
+                    input2+=3;
+                }
+                input2-=5;
+            }
+        }
+        return array;
+    }
     
     
     
